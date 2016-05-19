f21b0fed9ee3c70a7ec4039da774c713305ceded
akira inomata <akria003442@gmail.com>
2016-05-19 13:52:45 +0900

    form_testおしまい

---
+++ 01/form_test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+    <form action="form_test2.php" method="get">
+    <p>
+        <input type="text" name="nameInput">
+        <input type="submit">
+    </p>
+    </form>
+</body>
+</html>
